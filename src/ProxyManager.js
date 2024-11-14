21430aee5fb80f086b246c12aaf25c5e6a012303
NaN <73912873+NaN-05@users.noreply.github.com>
2024-11-15 02:27:18 +0700

    Update ProxyManager.js

--- src/ProxyManager.js
+++ src/ProxyManager.js
@@ -3,12 +3,7 @@ const axios = require('axios');
 const fs = require('fs');
 
 const PROXY_SOURCES = {
-  'SERVER 1': 'https://files.ramanode.top/airdrop/grass/server_1.txt',
-  'SERVER 2': 'https://files.ramanode.top/airdrop/grass/server_2.txt',
-  'SERVER 3': 'https://files.ramanode.top/airdrop/grass/server_3.txt',
-  'SERVER 4': 'https://files.ramanode.top/airdrop/grass/server_4.txt',
-  'SERVER 5': 'https://files.ramanode.top/airdrop/grass/server_5.txt',
-  'SERVER 6': 'https://files.ramanode.top/airdrop/grass/server_6.txt',
+  'Ngaret': 'https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text','https://github.com/monosans/proxy-list/raw/main/proxies/all.txt',
 };
 
 async function fetchProxies(url) {
